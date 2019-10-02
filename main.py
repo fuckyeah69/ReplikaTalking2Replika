a0d932b8d4f3a2667d5aa2e6fc5f202c42367cc2
Karim Saad <karimsaad@gmx.net>
2019-10-03 00:42:16 +0200

    Commenting Stuff <3

--- main.py
+++ main.py
@@ -10,12 +10,15 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import time
 
+# Defs (Browser "Windows")
 browser = webdriver.Chrome()
 browser2 = webdriver.Chrome()
 
+# Global Messages Containers (idk if it's used in the current version)
 globalMessagesRep1 = [] # first Replika
 globalMessagesRep2 = [] # second Replika
 
+# Automated Login, please don't abuse it!
 def DoLogin(browser, username, password):
     #emailOrPhone - input 
     #login-password - input 
@@ -34,6 +37,7 @@ def DoLogin(browser, username, password):
     except:
         pass
 
+# Sends a Message by typing the text by "send_keys"
 def SendMessage(browser, text):
     #send-message-textarea
     try:
@@ -45,6 +49,7 @@ def SendMessage(browser, text):
         print("err with sending msg")
         pass
 
+# Poor Method for getting all Messages out of Selenium Window... 
 def GetMessages(browser, messages=[]):
     try:
         cid = "ChatMessagesList__ChatMessagesListInner-sc-1ajwmer-1"
@@ -60,12 +65,14 @@ def GetMessages(browser, messages=[]):
     except:
         pass
 
+# Gets TheLastMessage out of Array messages
 def GetLastMessage(browser, messages=[]):
     try:
         return messages[len(messages)-1]
     except:
         pass
 
+# Does the reading messages from each window and sending stuff
 def DoMessageLoop(browser, browser2):
     lastlastmessage1 = ""
     lastlastmessage2 = ""
@@ -73,9 +80,11 @@ def DoMessageLoop(browser, browser2):
     lastSame2 = False
 
     while True:
+        #Getting current Messages (for checking if any new messages)
         r1 = GetMessages(browser, globalMessagesRep1)
         r2 = GetMessages(browser2, globalMessagesRep2)
 
+        # Getting of each messages array the last message (usually it's the newest)
         lmg1 = GetLastMessage(browser, r1)
         lmg2 = GetLastMessage(browser2, r2)
 
@@ -84,6 +93,7 @@ def DoMessageLoop(browser, browser2):
         lastSame1 = True
         lastSame2 = True
 
+        # Checking if something changed, if yes doing the stuff like sending it etc.
         if lmg1 != lastlastmessage1:
             SendMessage(browser2, lmg1)
             print("Sended" , lmg2)
@@ -108,6 +118,7 @@ DoLogin(browser2, "username2", "password")
 import _thread
 _thread.start_new_thread(DoMessageLoop, (browser, browser2,))
 
+# Waiting for input after loop "finished"
 input("k")
 browser.close()
 browser2.close()
